4fb4288adfd4f4c1f084e6c6921dfcfd10e05a5c
Maxim Baglyuk <dlyamusora124@gmail.com>
2026-05-13 23:27:31 +0300

    1

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -5,17 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // доступ к единственной корзине (Singleton)
+
         Basket basket = Basket.getInstance();
 
         Item freshMilk = ProductFactory.createFood("Молоко", 100, 1.0);
         LocalDate expirationDate = LocalDate.now().plusDays(1);
 
-        // оборачиваем молоко в декоратор для расчета скидки
         Item discountMilk = new ExpirationDiscountDecoratorItem(freshMilk, expirationDate);
         basket.addItem(discountMilk);
 
-        // работа Компоновщика
         BundleItem giftBox = new BundleItem("Подарочный набор 'Чаепитие'");
 
         giftBox.add(ProductFactory.createFood("Печенье овсяное", 60, 0.4));
@@ -23,15 +21,12 @@ public class Main {
 
         basket.addItem(giftBox);
 
-        // вывод списка покупок и расчет
         double totalSum = 0;
         System.out.println("СПИСОК ПОКУПОК В КОРЗИНЕ:");
 
-        // проходим по корзине (Iterator)
         for (Item item : basket.getItemList()) {
             System.out.println(item.getName() + " - " + item.getPrice() + " руб.");
 
-            // проверяем наличие вложенных товаров
             List<Item> innerItems = item.getChildItemList();
             if (!innerItems.isEmpty()) {
                 for (Item inner : innerItems) {
@@ -41,7 +36,6 @@ public class Main {
             totalSum += item.getPrice();
         }
 
-        // применение итоговой скидки (Strategy)
         Discount studentDiscount = new StudentDiscount();
         double finalSum = studentDiscount.calculate(totalSum);
 
